43fb69fc3779880210e355bc2f8dfb0a7ba2a6ad
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 17:56:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+	event.preventDefault()
+	const formData = new FormData(event.target)
+	const f = formData.get("fname")
+	const l = formData.get("lname")
+	alert(f + " " + l)
 }
